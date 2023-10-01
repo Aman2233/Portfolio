78bf04c55b5bf7490ea2c4565c15ba50312b9cc4
Amanueal Mamo <95701127+Aman2233@users.noreply.github.com>
2023-10-02 03:50:48 +1030

    Added sections

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
-import {LiaCatSolid} from 'react-icons/lia';
-import aman from '/public/aman.JPG';
 
 
 function Homepage() {
@@ -10,13 +8,13 @@ function Homepage() {
       <main className=' bg-white px-10'>
           <section className='min-h-screen'>
               <nav className='py-10 mb-12 flex justify-between'>
-                <h1>Amanueal</h1>
+                <h1></h1>
                   <ul className='flex  items-center'>
                       <li>
                           <BsFillMoonStarsFill classname= 'cursor-pointer text-2xl'/>
                       </li>
                       <li>
-                          <a className='bg-gradient-to-r from-lime-600 to-lime-600 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
+                        <a className='bg-gradient-to-r from-lime-600 to-lime-600 text-white px-4 py-2 rounded-md ml-8'>Resume</a>
                       </li>
                   </ul>
               </nav>
@@ -74,9 +72,74 @@ function Homepage() {
 
             </div>
             <div>
-                <div>
+                  <div className='text-center shadow-lg p-10 rounded-xl my-10 '>
+                      <img src="/tick-dynamic-color.png" className='w-32 h-32 flex mx-auto' />
+                      <h3 className='text-lg font-medium pt-8 pb-2'>Skills</h3>
+                      <p className='py-2'>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies massa diam,
+                          at porta elit commodo pellentesque. Mauris rhoncus, dui eu scelerisque pretium, dui odio
+                          tempor augue, molestie rutrum diam magna sed nulla. Cras volutpat risus ac justo hendrerit maximus.
+                          Pellentesque sollicitudin vehicula consequat.
 
-                </div>
+                      </p>
+                      <h4 className='py-4 text-lime-600'>Design Tools i use</h4>
+                      <p className='text-gray-600 py-1 '>Tools</p>
+                      <p>Tools</p>
+                      <p>Tools</p>
+
+
+
+                  </div>
+
+                  <div className='text-center shadow-lg p-10 rounded-xl my-10 '>
+                      <img src="/travel-dynamic-color.png" className='w-32 h-32 flex mx-auto' />
+                      <h3 className='text-lg font-medium pt-8 pb-2'>Certs</h3>
+                      <p className='py-2'>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies massa diam,
+                          at porta elit commodo pellentesque. Mauris rhoncus, dui eu scelerisque pretium, dui odio
+                          tempor augue, molestie rutrum diam magna sed nulla. Cras volutpat risus ac justo hendrerit maximus.
+                          Pellentesque sollicitudin vehicula consequat.
+
+                      </p>
+                      <h4 className='py-4 text-lime-600'>Design Tools i use</h4>
+                      <p className='text-gray-600 py-1 '>Tools</p>
+                      <p>Tools</p>
+                      <p>Tools</p>
+
+
+
+                  </div>
+
+                  <div className='text-center shadow-lg p-10 rounded-xl my-10 '>
+                      <img src="/sun-dynamic-color.png" className='w-32 h-32 flex mx-auto' />
+                      <h3 className='text-lg font-medium pt-8 pb-2'>Working On</h3>
+                      <p className='py-2'>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies massa diam,
+                          at porta elit commodo pellentesque. Mauris rhoncus, dui eu scelerisque pretium, dui odio
+                          tempor augue, molestie rutrum diam magna sed nulla. Cras volutpat risus ac justo hendrerit maximus.
+                          Pellentesque sollicitudin vehicula consequat.
+                      </p>
+                      <h4 className='py-4 text-lime-600'>Design Tools i use</h4>
+                      <p className='text-gray-600 py-1 '>Tools</p>
+                      <p>Tools</p>
+                      <p>Tools</p>
+
+
+
+                  </div>
+
+                  <section>
+                    <div>
+                        <h3 className='text-3xl py-1 '>Portfolio</h3>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies massa diam,
+                          at porta elit commodo pellentesque. Mauris rhoncus, dui eu scelerisque pretium, dui odio
+                          tempor augue, molestie rutrum diam magna sed nulla. Cras volutpat risus ac justo hendrerit maximus.
+                          Pellentesque sollicitudin vehicula consequat.
+
+                        </p>
+                    </div>
+                  </section>
             </div>
           </section>
       </main>
